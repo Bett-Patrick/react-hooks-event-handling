8d58fcf51458422f79b4fd659f391e47d9bf7714
Bett <bettpatrick080@gmail.com>
2024-02-07 02:16:17 +0300

    Completed Assignment

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -7,9 +7,10 @@ function MultiButton() {
 
   return (
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      {/* By writing out an arrow function here, we're providing each of our button's onClick handlers a function definition that will only be invoked when the button is clicked. */}
+      <button onClick={()=>handleClick(1)}>Button 1</button>
+      <button onClick={()=>handleClick(2)}>Button 2</button>
+      <button onClick={()=>handleClick(3)}>Button 3</button>
     </div>
   );
 }
